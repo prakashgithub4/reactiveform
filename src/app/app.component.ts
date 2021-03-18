2fdd54d29a214b99ad77984d78297e57e36a37ba
Prakash Chandra Paramanick <I0445@ivan.com>
2021-03-18 20:43:49 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactiveForm';
+  form = new FormGroup({
+    name: new FormControl('',[Validators.required,Validators.minLength(2)]),
+    email:new FormControl("",[Validators.required,Validators.email]),
+    age: new FormControl('',[Validators.required])
+  });
+  get formdata(){
+    return this.form.controls;
+  }
+  onSubmit(){
+   console.log(this.form.value);
+   
+  }
 }
